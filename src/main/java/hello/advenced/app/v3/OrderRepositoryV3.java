036dcec97ae51ef47cdd8504e4644f9de45ab2b9
biznet <biznet@naonsoft.com>
2024-02-23 17:59:49 +0900

    쓰레드로컬

---
+++ src/main/java/hello/advenced/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,40 @@
+package hello.advenced.app.v3;
+
+import hello.advenced.trace.TraceId;
+import hello.advenced.trace.TraceStatus;
+import hello.advenced.trace.hellotrace.HelloTraceV2;
+import hello.advenced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV3 {
+    private final LogTrace trace;
+
+    public void save(String itemId){
+
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderRepository.save()");
+            //저장로직
+            if(itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        }catch(Exception e){
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/hello/advenced/app/v3/OrderServiceV3.java
@@ -0,0 +1,30 @@
+package hello.advenced.app.v3;
+
+import hello.advenced.trace.TraceId;
+import hello.advenced.trace.TraceStatus;
+import hello.advenced.trace.hellotrace.HelloTraceV2;
+import hello.advenced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId){
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        }catch(Exception e){
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+
+
+}
